b6a0bc6fea83cd7af5263c3aa28aa548052123ce
Andrew Yolland <and.t.yoll@gmail.com>
2015-01-29 16:55:44 -0600

    broke the driver by trying to separate a invalid-input-loop into it's own method.

--- rockpaperscissor.rb
+++ rockpaperscissor.rb
@@ -13,25 +13,37 @@ class Player
   end
   
   def set_throw(throw_name)
-    @throw = 0 if throw_name.upcase == 'ROCK' 
-    @throw = 1 if throw_name.upcase == 'PAPER'
-    @throw = 2 if (throw_name.upcase == 'SCISSOR') || (throw_name.upcase == 'SCISSORS')
+    @throw = 0 if throw_name == 'ROCK' 
+    @throw = 1 if throw_name == 'PAPER'
+    @throw = 2 if (throw_name == 'SCISSOR')
+    @throw
   end
   
   def ai_random
     random_throw = Random.new
     @throw = random_throw.rand(3)
+    @throw
   end
   
 end
 
 class Match
   
+  attr_reader :player1, :player2, :win_at
+  
   def initialize(player1,player2,win_condition)
     @player1 = player1
-    @player2 = player 2
-    @win_at = win_condition.to_i
-    @win_at = 5 if @win_at > 5
+    @player2 = player2
+    @win_at = win_condition
+    @p1points = 0
+    @p2points =0
+  end
+  
+  def test_winner
+    winner = nil
+    winner = @player1.name if @p1points == @win_at
+    winner = @player2.name if @p2points == @win_at
+    winner
   end
   
 end
@@ -49,21 +61,40 @@ def driver_ask_bot(pstring)
   puts "Is Player-" + pstring +" a HUMAN or COMPUTER?"
   control = gets.chomp.upcase
   control = nil if !((control == "HUMAN")||(control == "COMPUTER"))
-  puts "Sorry. One more time:" if control == nil
+  driver_wrong_input(control)
   control
+end
+
+def driver_wrong_input_loop(input_method,input)
+  notnil = input_method(input)
+  while notnil == nil
+    notnil = input_method(input)
+  end
+end
+
+def driver_wrong_input(input)
+    puts "Sorry. One more time:" if input == nil
 end
 
 #uses driver_ask_name and driver_ask_bot to build a new Player object
 
 def driver_build_player(pstring)
   pname = driver_ask_name(pstring)
-  pbot = driver_ask_bot(pstring)
-  while pbot == nil
-    pbot = driver_ask_bot(pstring)
-  end
+  driver_wrong_input_loop(driver_ask_bot,pstring)
   Player.new(pname,pbot)
 end
 
+def d_line
+  puts "-" * 40
+end
+
+def driver_human_set_throw(player)
+  puts player.name + ": select your throw! (ROCK, PAPER, OR SCISSOR)"
+  throw = gets.chomp.upcase
+  throw = nil if !(((throw == 'ROCK')||(throw == 'PAPER'))||(throw == 'SCISSOR'))
+  driver_wrong_input(throw)
+  throw
+end
 
 #main driver, sets up two player objects
 
@@ -71,8 +102,17 @@ def driver
   puts "This is a Rock Paper Scissors game."
   p1 = driver_build_player('One')
   p2 = driver_build_player('Two')
+  puts "How many points will you play to? (Between 1-5)"
+  length = gets.chomp.to_i
+  length = 1 if length < 1
+  length = 5 if length > 5
   puts 'Okay!'
+  d_line
   puts p1.name + ": " + p1.control + " >VERSUS< " + p2.name + ": " + p2.control
+  this_match = Match.new(p1,p2,length)
+  d_line
+  p1.set_throw(driver_human_set_throw(p1)) if p1.control == "HUMAN"
+  
 end
 
 binding.pry
